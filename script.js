1051e0599b163320e69100cfc5786cd42ba5237c
ameliapham <ptt.huong.8895@gmail.com>
2024-01-16 03:17:45 +0100

    Create variables and a function click for buttons

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 import "./style.css"
+
+const buttons = document.querySelectorAll(".btn")
+const slides = document.querySelectorAll(".slide")
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+
+    })
+})
