c6d8df98cb0361867f699d1098c67a0eb5dcc6ce
ameliapham <ptt.huong.8895@gmail.com>
2024-01-16 03:51:53 +0100

    Adjust newIndex if it's out of range

--- script.js
+++ script.js
@@ -16,6 +16,14 @@ buttons.forEach((button) => {
         newIndex = calcNextSlide + [...slides].indexOf(slideActive)
         console.log(newIndex)
 
+        if (newIndex < 0) {
+            newIndex = [...slides].length - 1
+        }
+
+        if (newIndex >= [...slides].length) {
+            newIndex = 0
+        }
+
         slides[newIndex].classList.add("active")
         slideActive.classList.remove("active")
 
