9b4c2650a7b9ab6ae3c360b4742686dda7d4f525
ameliapham <ptt.huong.8895@gmail.com>
2024-01-16 03:21:46 +0100

    Add calcNextSlide to calcul the value of each buttons

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const slides = document.querySelectorAll(".slide")
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
+        const calcNextSlide = e.currentTarget.id === "next" ? 1 : -1;
+        console.log("calcNextSlide : ", calcNextSlide)
 
     })
 })
