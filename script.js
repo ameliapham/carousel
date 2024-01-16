8e452bd1468d96ae8e770b1fdb82324e28708dbb
ameliapham <ptt.huong.8895@gmail.com>
2024-01-16 04:22:42 +0100

    Comment all the console.log

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const slides = document.querySelectorAll(".slide")
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         const calcNextSlide = e.currentTarget.id === "next" ? 1 : -1;
-        console.log("calcNextSlide : ", calcNextSlide)
+        // console.log("calcNextSlide : ", calcNextSlide)
 
 
         const slideActive = document.querySelector(".active")
-        console.log("slideActive index : ", [...slides].indexOf(slideActive))
+        // console.log("slideActive index : ", [...slides].indexOf(slideActive))
 
         let newIndex
         newIndex = calcNextSlide + [...slides].indexOf(slideActive)
-        console.log(newIndex)
+        // console.log(newIndex)
 
         if (newIndex < 0) {
             newIndex = [...slides].length - 1
